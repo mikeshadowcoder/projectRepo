17637e046ceeb3698bdb7a780d570fe469efb3eb
llop <llop.omongos@gmail.com>
2023-05-15 01:34:28 +0800

    Changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +22,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 console.log(app)
+const db = getFirestore(app);
+
